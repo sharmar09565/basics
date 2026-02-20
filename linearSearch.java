67c7b4d0e6b0468220e875e39068380315eb46c0
sharmar09565 <sharmar09565@gmail.com>
2026-02-20 23:25:42 +0530

    ajs

--- linearSearch.java
+++ linearSearch.java
@@ -1,18 +1,21 @@
 import java.util.*;
 public class linearSearch{
-    Scanner sc = new Scanner();
-    
-    int n = sc.nextInt();
     public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        boolean isFound = false;
+
         int arr[] = {21,45,16,81,44};
         for(int i=0;i<arr.length;i++){
             if(arr[i]==n){
                 System.out.println("Element present at "+(i+1)+" position");
-            }
-            else{
-                System.out.println("Not found");
+                isFound = true;
+                break;
             }
 
+        }
+        if(!isFound){
+            System.out.print("Not found");
         }
     }
 }
