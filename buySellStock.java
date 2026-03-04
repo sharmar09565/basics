73171bdac88e3b57f4f8102c846867ab8a68156b
sharmar09565 <sharmar09565@gmail.com>
2026-03-04 22:16:49 +0530

    debugged

--- buySellStock.java
+++ buySellStock.java
@@ -1,23 +1,21 @@
 public class buySellStock {
     public static void maxProfit(int arr[]){
-        int CP,profit=0,buyDate=0,sellDate=0;
+        int profit=0,maxProfit = 0,buyDate=0,sellDate=0;
         int BP = Integer.MAX_VALUE;
-        int SP = Integer.MIN_VALUE;
         for(int i=1;i<arr.length;i++){
-
-            BP = Math.min(arr[i-1], BP);
-            System.out.println("BP="+BP);
-            SP = Math.max(arr[i], SP);
-            
-            profit = Math.max(SP-BP, profit);
-            if(BP == arr[i-1]){
-                buyDate = i;
+            if(BP<arr[i]){  // is today's price is higher than Buy price
+                profit = arr[i] - BP;   // today's profit
+                if(profit>maxProfit){
+                    maxProfit = profit;
+                    sellDate = i+1;
+                }
+                
             }
-            if(SP == arr[i]){
-                sellDate = i+1;
+            else{   // if not then price is lower than before
+                BP = arr[i];    // so change th BP to new buy price
+                buyDate = i+1;
             }
-            
-        }
+        }            
         System.out.println("Buying date: "+ buyDate);
         System.out.println("Selling date: "+ sellDate);
         System.out.println(profit);
--- maxSubArr.java
+++ maxSubArr.java
@@ -1,19 +1,17 @@
 public class maxSubArr {
     // Brute force approach
-    // public static void subArrays(int arr[]){
+    // public static void subArrays(int arr[]) {
     // int sum = 0, largestSum = Integer.MIN_VALUE;
-    // for(int i=0;i<arr.length;i++){
-    // for(int j=i;j<arr.length;j++){
-    // for(int k=i;k<=j;k++){
-    // System.out.print(arr[k]+ " ");
-    // sum+=arr[k];
+    // for (int i = 0; i < arr.length; i++) {
+    // for (int j = i; j < arr.length; j++) {
+    // for (int k = i; k <= j; k++) {
+    // sum += arr[k];
 
     // }
-    // if(largestSum<sum){
-    // largestSum=sum;
+    // if (largestSum < sum) {
+    // largestSum = sum;
     // }
-    // System.out.println(" = "+sum+" Largest = "+ largestSum);
-    // sum=0;
+    // sum = 0;
     // }
     // System.out.println();
     // }
@@ -21,27 +19,27 @@ public class maxSubArr {
     // }
 
     // prefix sum method
-    // public static void subArrays(int arr[]){
+    // public static void subArrays(int arr[]) {
     // int sum = 0, largestSum = Integer.MIN_VALUE;
     // int preFix[] = new int[arr.length]; // Make a newArr that store sum of sub
     // arr
-    // preFix[0]=arr[0];
 
-    // for(int i=1;i<preFix.length;i++){
-    // preFix[i] = preFix[i-1] + arr[i];
+    // preFix[0] = arr[0];
+
+    // for (int i = 1; i < preFix.length; i++) {
+    // preFix[i] = preFix[i - 1] + arr[i];
     // }
 
-    // for(int i=0;i<arr.length;i++){
-    // for(int j=i;j<arr.length;j++){
-    // if (i==0) {
+    // for (int i = 0; i < arr.length; i++) {
+    // for (int j = i; j < arr.length; j++) {
+    // if (i == 0) {
     // sum = preFix[j];
+    // } else {
+    // sum = preFix[j] - preFix[i - 1];
     // }
-    // else{
-    // sum = preFix[j]+preFix[i-1];
     // }
-    // }
-    // if(largestSum<sum){
-    // largestSum=sum;
+    // if (largestSum < sum) {
+    // largestSum = sum;
     // }
     // }
     // System.out.println(largestSum);
@@ -50,29 +48,17 @@ public class maxSubArr {
     // Kadane's Method
     public static void subArrays(int arr[]) {
         int curSum = 0, largestSum = Integer.MIN_VALUE;
-        int preFix[] = new int[arr.length]; // Make a newArr that store sum of sub arr
-        preFix[0] = arr[0];
-
-        for (int i = 1; i < preFix.length; i++) {
-            preFix[i] = preFix[i - 1] + arr[i];
-        }
 
         for (int i = 0; i < arr.length; i++) {
-            curSum+=arr[i];
-            if (curSum<0) {
-                curSum=0;
+            curSum += arr[i];
+            if (curSum < 0) {
+                curSum = 0;
             }
-            
+            largestSum = Math.max(largestSum, curSum);
         }
-        // if (largestSum < curSum) {
-        //     largestSum = curSum;
-        // }
-        largestSum = Math.max(largestSum, curSum);
+
         System.out.println(largestSum);
     }
-    
-
-
 
     public static void main(String args[]) {
         int arr[] = { 1, -2, -3, 4, 5 };
